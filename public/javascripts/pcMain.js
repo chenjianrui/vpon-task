31dee4b70969c1f6583503c4f2335ae192b97fd6
chenjianrui <sky75217@gmail.com>
2019-09-20 14:40:25 +0800

    再加上一層是否用 safari 開啟的

--- public/javascripts/pcMain.js
+++ public/javascripts/pcMain.js
@@ -23,12 +23,16 @@ ws.onclose = () => {
 ws.onmessage = event => {
   const audio = document.querySelector(`audio[data-key="${event.data}"]`);
   const key = document.querySelector(`div[data-key="${event.data}"]`);
-  const promise = document.querySelector('audio').play();
-  // 判斷 safari 是否無法自動播放
-  if (promise !== undefined) {
-    promise.catch(error => {
-      alert('請前往設定開啟自動播放。');
-    });
+  const isSafariBrowser =
+    /Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent);
+  if (isSafariBrowser) {
+    const promise = document.querySelector('audio').play();
+    // 判斷 safari 是否無法自動播放
+    if (promise !== undefined) {
+      promise.catch(error => {
+        alert('請前往設定開啟自動播放。');
+      });
+    }
   }
   if (!audio) return;
   key.classList.add('playing');
